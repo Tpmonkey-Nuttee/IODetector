cf4e5adff54926417fbc3a52e7ae535de2eb8418
Tpmonkey <56111402+Tpmonkey-Nuttee@users.noreply.github.com>
2021-06-21 21:59:48 +0700

    Update model.py

--- model.py
+++ model.py
@@ -6,16 +6,23 @@ import re
 import string
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
 class Model:
     def __init__(self) -> None:
         self.model = pickle.load(open("gnb.pickle", "rb"))
         self.th_stop = pythainlp.corpus.common.thai_stopwords() 
+        
+        df = pd.read_csv("dataset.csv")
+        features_train, features_test, labels_train, labels_test = train_test_split(df['text'], df['is_spam'], test_size=0.3, random_state = 42)
+        
         self.vectorizer = TfidfVectorizer(
             analyzer = 'word',  # default settings
             tokenizer= self.__split, # using our own tokenizer
             token_pattern = None # I don't know what is this, but I will put it anyways.
         )
+        self.vectorizer.fit(features_train)
   
     def __split(self, text: str):
         # tokenize text using pythainlp tokenizer
@@ -53,7 +60,7 @@ class Model:
         text = self.__clean(text)
         if text.strip() == "": return False
         
-        vectorized = self.vectorizer.transform(text)
+        vectorized = self.vectorizer.transform( [text] )
         prediction = self.model.predict( vectorized.toarray() )
         
         return prediction[0] == 1
